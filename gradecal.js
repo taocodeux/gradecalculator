11a44af387428e5731810e9a2edbd07318d6ba81
taocodeux <taofeekatb1@gmail.com>
2023-08-09 12:59:39 +0100

    grade checker

---
+++ gradecal.js
@@ -0,0 +1,17 @@
+function checkResult(){
+    let myPercentage = document.getElementById("inputgrade").value
+    let myGradePoint = document.getElementById("result")
+
+    if (myPercentage < 50) {
+        result.innerHTML = "F9 - Failed &#x1F641"
+        }
+    else if (myPercentage >= 50 && myPercentage < 65){
+        result.innerHTML = "C - Credit &#x1F600";
+        }
+    else if (myPercentage >= 65 && myPercentage < 80){
+        result.innerHTML = "B - Passed &#x1F601";
+        }
+    else if (myPercentage >= 80 && myPercentage <= 100){
+        result.innerHTML = "A - Excellent &#x1F603";
+        }
+   }
