35b590b9149a89494f042324699ff963465c7676
Bamidele Badmus Taofikat <taofeekatb1@gmail.com>
2025-04-17 21:15:21 +0100

    ready for deploying

--- gradecal.js
+++ gradecal.js
@@ -1,20 +1,35 @@
 function checkResult(){
-    let myPercentage = document.getElementById("inputgrade").value
+    let inputValue = document.getElementById("inputgrade").value
     let myGradePoint = document.getElementById("result")
 
-    if (myPercentage < 50) {
-        result.innerHTML = "F9 - Failed &#x1F641"
+    
+    if (inputValue === "") {
+        myGradePoint.innerHTML = "Please input your grade score!"
+        return
+    }
+
+    let myPercentage = Number(inputValue)
+
+    if (myPercentage >= 0 && myPercentage < 50) {
+        myGradePoint.innerHTML = "F9 - Failed &#x1F641"
         }
     else if (myPercentage >= 50 && myPercentage < 65){
-        result.innerHTML = "C - Credit &#x1F600";
+        myGradePoint.innerHTML = "C - Credit &#x1F600"
         }
     else if (myPercentage >= 65 && myPercentage < 80){
-        result.innerHTML = "B - Passed &#x1F601";
+        myGradePoint.innerHTML = "B - Passed &#x1F601"
         }
     else if (myPercentage >= 80 && myPercentage <= 100){
-        result.innerHTML = "A - Excellent &#x1F603";
+        myGradePoint.innerHTML = "A - Excellent &#x1F603"
         }
     else if(myPercentage >100){
-        alert("grade cannot be greater than 100")
+        myGradePoint.innerHTML="grade cannot be greater than 100 &#x1F603"
     }
+    else if(myPercentage === ""){
+        myGradePoint.innerHTML ="Please input your grade score"
+    }
+    else {
+        myGradePoint.innerHTML = "Invalid input!"
+    }
+    
    }
